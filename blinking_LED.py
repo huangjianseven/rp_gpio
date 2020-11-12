16640dd67aa0a574933796243d50e5eab39fd43e
Jian Huang <57820048@qq.com>
2020-11-12 16:50:43 +0000

    Blinking LED w/ RPi.GPIO

---
+++ blinking_LED.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO 
+from time import sleep 
+GPIO.setwarnings(False) 
+ 
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(17, GPIO.OUT) 
+ 
+while True: 
+    GPIO.output(17, True) 
+    sleep(1) 
+    GPIO.output(17, False) 
+    sleep(1) 
